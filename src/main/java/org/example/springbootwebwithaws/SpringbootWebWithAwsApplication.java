7d07082430daeded498b1fd8985b21e602ecde10
KyungYiHyun <wm0755123@naver.com>
2025-01-07 10:49:11 +0900

    Initial commit

---
+++ src/main/java/org/example/springbootwebwithaws/SpringbootWebWithAwsApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootwebwithaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebWithAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebWithAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootwebwithaws/SpringbootWebWithAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootwebwithaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebWithAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
